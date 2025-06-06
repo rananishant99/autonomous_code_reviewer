433d3e2fd005455695a634d8945aa8950bd60bb3
Nishant <nishant.rana@crossml.com>
2025-06-06 11:48:13 +0530

    Added helper files

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 class UserSignupSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
-    email = serializers.EmailField(min_length=20)
+    email = serializers.EmailField(max_length=20)
 
     class Meta:
         model = User
--- authentication/views.py
+++ authentication/views.py
@@ -3,17 +3,27 @@ from rest_framework.generics import GenericAPIView
 from .serializers import UserSignupSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from autonomous_code_reviewer.utils import create_api_response, create_serializer_response
+from autonomous_code_reviewer.constants import ActionMessages
+from .constants import Authentication
 
 class UserSignupView(GenericAPIView):
     serializer_class = UserSignupSerializer
-
     def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            return Response({
-                "status": "success",
-                "user_id": user.id,
-                "email": user.email
-            }, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            serializer = self.get_serializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return create_api_response(
+                    status_code=status.HTTP_201_CREATED,
+                    message=Authentication.SIGNUP['CREATED'],
+                )
+            return create_api_response(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                message=create_serializer_response(serializer.errors),
+            )
+        except Exception as e:
+            return create_api_response(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                message=ActionMessages.COMMON['SERVER_ERROR']
+            )
